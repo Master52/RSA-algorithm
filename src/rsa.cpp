96467c345f79229e3a95590d36642b05d6f3d005
Master52 <harajmast9008@gmail.com>
2020-04-03 20:57:44 +0530

    Fixed random generate bug; Added Functionality of genrating public and private key

--- src/rsa.cpp
+++ src/rsa.cpp
@@ -2,11 +2,30 @@
 
 
 RSA::RSA() {
-    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    engine.seed(seed);
+    std::random_device rd;
+    engine.seed(rd());
     auto nums = generateNumbers();
-    fprintf(stdout,"First num %ld \n",nums.first);
-    fprintf(stdout,"Second num %ld  \n",nums.second);
+
+    p_num1 = nums.first;
+    p_num2 = nums.second;
+
+    N = p_num1 * p_num2;
+
+    PHI = (p_num1 - 1) * (p_num2 -1 );
+
+    E_KEY = generateEncKey();
+
+    D_KEY = generateDecKey(E_KEY,PHI);
+
+
+#ifdef __DEBUG__
+    fprintf(stdout,"p_num1 = %ld \n",p_num1);
+    fprintf(stdout,"p_num2 = %ld  \n",p_num2);
+    fprintf(stdout,"N = %ld  \n",N);
+    fprintf(stdout,"PHI =  %ld  \n",PHI);
+    fprintf(stdout,"E_KEY =   %ld  \n",E_KEY);
+    fprintf(stdout,"D_KEY =   %ld  \n",D_KEY);
+#endif
 
 
 }
@@ -16,20 +35,20 @@ bool RSA :: isPrime(const ul& num)
     if(num < 2) return false;
     if(num == 2) return true;
     if((num & 0x01) == 0) return false;
-    for(ul i = 0, n = std::sqrt(num) ; i <= n ; ++i)
+    for(ul i = 3, n = std::sqrt(num) ; i <= n ; i = i + 2)
         if( num % i == 0) return false;
 
     return true;
 
 }
 
-ul RSA :: getPrimeNumber()
+ul RSA :: getPrimeNumber(const int& lower,const int& upper,const int& offset)
 {
-    std::uniform_int_distribution<ul> dist(1000,1000000);
+    std::uniform_int_distribution<ul> dist(lower,upper);
     bool primeFound = false;
     ul num;
     while(!primeFound) {
-        num = dist(engine);
+        num = dist(engine) + offset;
         if(isPrime(num))
             primeFound = true;
     }
@@ -39,7 +58,7 @@ ul RSA :: getPrimeNumber()
 
 std::pair<ul,ul> RSA :: generateNumbers()
 {
-    return (std::make_pair(getPrimeNumber(),getPrimeNumber()));
+    return (std::make_pair(getPrimeNumber(1,32767,4096),getPrimeNumber(1,61440,2)));
 }
 
 std::string RSA :: encrypt(const std::string& message)
@@ -55,6 +74,45 @@ std::string RSA :: decrypt(const std::string& encryptText)
 
 }
 
+ul RSA :: generateEncKey()
+{
+    std::uniform_int_distribution<ul> dist(1,61440);
+    ul e = dist(engine) + 4096;
+    while( e < PHI ) {
+        if( gcd(e,PHI) == 1) break;
+        else ++e;
+    }
+
+    return e;
+
+}
+
+int RSA :: gcd (ul e,ul phi)
+{
+    ul temp;
+    while( true ) {
+        temp = e % phi;
+        if( temp == 0 ) {
+            return phi;
+        }
+        e = phi;
+        phi = temp;
+    }
+
+}
+ul RSA :: generateDecKey(ul e, ul phi)
+{
+    e = e % phi;
+    ul d = 0;
+    for (ul i = 1 ; i < phi ; ++i)
+       if ((e * i) %  phi == 1)
+       {
+          d =  i;
+          break;
+       }
+    return d;
+
+}
 RSA :: ~RSA()
 {
 
--- src/rsa.hpp
+++ src/rsa.hpp
@@ -6,14 +6,33 @@
 #include <chrono>
 #include <utility>
 
+#define __DEBUG__
+
 typedef unsigned long ul;
 
 class RSA {
     private:
         std::mt19937 engine;
+
+        /* Prime Numbers */
+        ul p_num1 = 0, p_num2 = 0;
+
+        /* Used in a part for generating public Key*/
+        ul N = 0;
+
+        ul PHI = 0;
+
+        ul E_KEY = 0; // Encryption Key
+        ul D_KEY = 0; // Decryption Key
+
         bool isPrime(const ul& num);
-        ul getPrimeNumber();
+        ul getPrimeNumber(const int& lower,const int& upper,const int& offset);
         std::pair<ul,ul> generateNumbers();
+
+        int gcd(ul e,ul phi);
+        ul generateEncKey();
+        ul generateDecKey(ul e,ul phi);
+
 
 
     public:
